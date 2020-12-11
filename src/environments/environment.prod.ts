abe731493a375f78d4326826c1a2a77269d1e7f9
Jonathas Oliveira <68708850+Jonathas-Oliveira@users.noreply.github.com>
2020-12-11 21:20:32 -0200

    firs-commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyAhpBbklIOwQGq05zmR75kPVy1pPjnFOsE",
+    authDomain: "idealit-firebase.firebaseapp.com",
+    projectId: "idealit-firebase",
+    storageBucket: "idealit-firebase.appspot.com",
+    messagingSenderId: "18761394237",
+    appId: "1:18761394237:web:9776241cb9ce8d2bff6254"
+  }
 };
